5966532ff88f2bad5c8a67e9f9913867de14f5b7
Emil Privér <emil@rivercode.se>
2022-03-02 16:10:12 +0100

    init

---
+++ backend/database/data.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+}
