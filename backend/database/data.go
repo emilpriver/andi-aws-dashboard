0fdd3c622edfadd4a7cb945ed6d9d9782d04b5cd
Emil Privér <emil@rivercode.se>
2022-03-02 19:09:45 +0100

    init

--- backend/database/data.go
+++ backend/database/data.go
@@ -1,22 +1,48 @@
 package database
 
 import (
-	"gorm.io/driver/sqlite"
+	"fmt"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
-type Product struct {
+type GithubUser struct {
 	gorm.Model
-	Code  string
-	Price uint
+	GithubID int
+}
+
+type User struct {
+	gorm.Model
+	Email      string
+	Username   string
+	Avatar     string
+	GithubUser GithubUser
 }
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%S dbname=%S port=%s sslmode=disable TimeZone=Europe/Stockholm",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	// Migrate the schema
-	db.AutoMigrate(&Product{})
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		fmt.Print("Error on auto migrate")
+		return
+	}
+
+	err = db.AutoMigrate(&GithubUser{})
+	if err != nil {
+		fmt.Print("Error on auto migrate")
+		return
+	}
 }
--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,5 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.7
-	gorm.io/driver/sqlite v1.3.1
 	gorm.io/gorm v1.23.2
 )
